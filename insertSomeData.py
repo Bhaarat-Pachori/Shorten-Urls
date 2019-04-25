0522bbb15af9833f9bde3804906557103706deba
Bhaarat Pachori <vbhaarat90@gmail.com>
2019-04-24 22:02:41 -0400

    base code for shortening the urls

---
+++ insertSomeData.py
@@ -0,0 +1,16 @@
+def take_input(filename):
+    from createDBImportData import shortenSaveUrl
+    # Creating the class object to use
+    workwithurl = shortenSaveUrl()
+
+    # Read URLS from the input file
+    listurls = workwithurl.read_urls_from_file(filename)
+
+    # Make a a list of dictionaries from the inputs
+    formattedUrls = workwithurl.prepare_doc_to_insert(listurls)
+
+    shortenSaveUrl.creating_index()
+    # Insert the data into the database
+    shortenSaveUrl.insert_many_urls(formattedUrls)
+
+    shortenSaveUrl.imported = True
